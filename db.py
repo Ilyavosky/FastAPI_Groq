bb8bef2e9d702f25427ba3806276a622a8eef9cc
Ilya Cortés Ruiz <ilyacortesruiz@gmail.com>
2026-05-07 18:19:20 -0600

    Implementar archivo de conexión a Supabase

--- db.py
+++ db.py
@@ -8,8 +8,8 @@ url: str = os.environ.get("SUPABASE_URL")
 if not url:
     raise ValueError("SUPABASE_URL no fue configurada en las variables de entorno")
 
-api_key: str = os.environ.get("SUPABASE_KEY")
-if not api_key:
+key: str = os.environ.get("SUPABASE_KEY")
+if not key:
     raise ValueError("SUPABASE_KEY no fue configurada en las variables de entorno")
 
-supabase: Client = create_client(url, api_key)
+supabase: Client = create_client(url, key)
